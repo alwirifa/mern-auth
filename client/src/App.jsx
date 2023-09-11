d93f7369cfd7a125b76fa37e9652755901647ea7
Jihan Cahya <alwirifa611@gmail.com>
2023-09-12 01:22:54 +0700

    mern-auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import axios from "axios";
+import { UserContextProvider } from "./UserContext";
+import AccountPage from "./pages/AccountPage";
+import { Toaster } from "react-hot-toast";
+
+axios.defaults.baseURL = "mern-auth-alpha.vercel.app";
+axios.defaults.withCredentials = true;
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <UserContextProvider>
+      <Toaster position="top-center" />
+      <Routes>
+        <Route path="/" element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/account" element={<AccountPage />} />
+      </Routes>
+    </UserContextProvider>
+  );
+};
 
-export default App
+export default App;
