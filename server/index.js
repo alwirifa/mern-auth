59345fa94c356677fa98cba4f7d972b6a7e89955
Jihan Cahya <alwirifa611@gmail.com>
2023-09-12 01:33:14 +0700

    mern-auth

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: "https://mern-auth-client-drab.vercel.app/",
+    origin: "https://mern-auth-client-drab.vercel.app",
   })
 );
 
